9b90530c73529d5376bbc0c34594a60afc91db2c
Muhammad Muntazir Mehdi <mehdikhosa50@gmail.com>
2024-10-31 17:25:03 +0500

    MongoDB Connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,11 @@
 import mongoose from "mongoose"
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('MongoDB connected')
+    } catch (error) {
+        console.error(error.message)
+        process.exit(1)                     // 0 = Success, 1 = Failure
+    }
+}
