dc2527642e7e65c91918bcb27846b8737793bfe0
Muhammad Muntazir Mehdi <mehdikhosa50@gmail.com>
2024-10-31 17:26:04 +0500

    Server running Script

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
 import express from "express";
+
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.routes.js";
+
+const port = process.env.PORT || 5000;
+
+dotenv.config();
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+app.use("/api/products", productRoutes);  
+
+app.listen(port, () => {
+    connectDB();
+    console.log("Backend server is running at http://localhost:" + port + "/api/products !My favorite number is 42!");
+});
